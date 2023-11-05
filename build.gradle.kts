296176155c4eb1a0e1648224b8e194ed75002600
Jannis Hecheltjen <jannis.hecheltjen@gmx.de>
2023-11-05 22:41:03 +0100

    + changed description

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,7 +15,8 @@ labyMod {
         namespace = "nearplayerwidget"
         displayName = "NearPlayerWidget"
         author = "ByPander"
-        description = "Adds a widget that displays nearby players."
+        description = "Adds a widget that displays nearby players.\n" +
+                "You can use {name}, {displayname} and {distance} to format the textlines."
         minecraftVersion = "*"
         version = System.getenv().getOrDefault("VERSION", "1.0.0")
     }
