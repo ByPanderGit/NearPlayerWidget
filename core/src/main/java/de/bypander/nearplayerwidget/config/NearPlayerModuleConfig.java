42d479bd555cb1e9661349ddaa30f0a8dfc4c671
Jannis Hecheltjen <jannis.hecheltjen@gmx.de>
2024-06-25 16:10:15 +0200

    add icons to config

--- core/src/main/java/de/bypander/nearplayerwidget/config/NearPlayerModuleConfig.java
+++ core/src/main/java/de/bypander/nearplayerwidget/config/NearPlayerModuleConfig.java
@@ -4,37 +4,49 @@ import net.labymod.api.addon.AddonConfig;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SliderWidget.SliderSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.SwitchWidget.SwitchSetting;
 import net.labymod.api.client.gui.screen.widget.widgets.input.TextFieldWidget.TextFieldSetting;
+import net.labymod.api.configuration.loader.annotation.ConfigName;
+import net.labymod.api.configuration.loader.annotation.SpriteSlot;
 import net.labymod.api.configuration.loader.property.ConfigProperty;
 import net.labymod.api.configuration.settings.annotation.SettingSection;
 
+@ConfigName("settings")
 public class NearPlayerModuleConfig extends AddonConfig {
 
+  @SpriteSlot(x = 0, y = 0)
   @SwitchSetting
   private final ConfigProperty<Boolean> enabled = new ConfigProperty<>(true);
 
+  @SpriteSlot(x = 1, y = 0)
   @TextFieldSetting
   private final ConfigProperty<String> format = new ConfigProperty<>("{displayname} &7({distance}&7m)");
 
+  @SpriteSlot(x = 2, y = 0)
   @SliderSetting(steps = 1, min = 1, max = 25)
   private final ConfigProperty<Integer> maxEntries = new ConfigProperty<>(10);
 
+  @SpriteSlot(x = 3, y = 0)
   @SliderSetting(steps = 1, min = 0, max = 5)
   private final ConfigProperty<Integer> maxDecimals = new ConfigProperty<>(2);
 
   @SettingSection("color")
+  @SpriteSlot(x = 4, y = 0)
   @SwitchSetting
   private final ConfigProperty<Boolean> removeNameColor = new ConfigProperty<>(false);
 
+  @SpriteSlot(x = 5, y = 0)
   @SwitchSetting
   private final ConfigProperty<Boolean> coloredDistance = new ConfigProperty<>(true);
 
+  @SpriteSlot(x = 5, y = 0)
   @TextFieldSetting
   private final ConfigProperty<String> coloredDistanceText = new ConfigProperty<>("1024>&a;20>&6;10>&c");
 
   @SettingSection("removePlayerSection")
+  @SpriteSlot(x = 6, y = 0)
   @SwitchSetting
   private final ConfigProperty<Boolean> removePlayer = new ConfigProperty<>(false);
 
+  @SpriteSlot(x = 7, y = 0)
   @TextFieldSetting
   private final ConfigProperty<String> removePlayerText = new ConfigProperty<>("Name1;Name2;Name3");
 
@@ -52,7 +64,7 @@ public class NearPlayerModuleConfig extends AddonConfig {
   }
 
   public ConfigProperty<String> format() {
-    return  format;
+    return format;
   }
 
   public ConfigProperty<Boolean> coloredDistance() {
